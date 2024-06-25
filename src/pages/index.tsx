99a7aa6e3bfcfed01382308fe0c8262c4da44036
Francis Fernandes da Luz <francisluz@Franciss-MacBook-Pro-2.local>
2024-06-26 00:37:05 +0100

    Update meta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -249,7 +249,7 @@ export default function Page({
 
           <div className="w-full footer">
             <p className="text-center text-sm text-gray-500">
-              BeginID - Universal Wallet Address by <a href="https://begin.is">Begin Wallet</a>
+              BeginID - Universal Wallet Address</a>
             </p>
           </div>
 
