b05238ef2fe4e8f8fcedacf17a2240531d7fe55c
Francis Fernandes da Luz <francisluz@Franciss-MacBook-Pro-2.local>
2024-06-27 11:46:19 +0100

    update close btn

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -360,7 +360,7 @@ export default function Page({
                   <div className="flex items-center justify-between p-4 pb-2 md:p-5 md:pb-2 rounded-t dark:border-gray-600">
                     <button
                       type="button"
-                      className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                      className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-full text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
                       data-modal-hide="default-modal"
                       onClick={() => setSelectedNft(null)}
                     >
